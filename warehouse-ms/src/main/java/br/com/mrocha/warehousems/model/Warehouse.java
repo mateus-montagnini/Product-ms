300014861311de65973de00b21277604d64c7ff2
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-08-30 16:48:41 -0300

    Criando classe "Warehouse

---
+++ warehouse-ms/src/main/java/br/com/mrocha/warehousems/model/Warehouse.java
@@ -0,0 +1,21 @@
+package br.com.mrocha.warehousems.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "tb_warehouse")
+@Data
+public class Warehouse {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "id_product", nullable = false)
+    private Long productId;
+
+    @Column(name = "quantity")
+    private int quantity;
+}
