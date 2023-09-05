45199fa930adab44627c0ccb0125c06c0a15e3f1
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-09-05 16:22:08 -0300

    Criando metodo getById, delete e inactive na interface ProductService

--- product-ms/src/main/java/br/com/mrocha/productms/service/ProductService.java
+++ product-ms/src/main/java/br/com/mrocha/productms/service/ProductService.java
@@ -10,4 +10,10 @@ public interface ProductService {
     Optional<ProductDTO> create(ProductDTO request);
 
     List<ProductDTO> getAll();
+
+    Optional<ProductDTO> getById(Long id);
+
+    boolean inactive(Long id);
+
+    boolean delete(Long id);
 }
