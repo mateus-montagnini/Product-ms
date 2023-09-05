10cfa4959fd6119cc433e49840b9da0ac0b36743
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-09-05 15:58:14 -0300

    Criando metodo getAll na interface ProductService

--- product-ms/src/main/java/br/com/mrocha/productms/service/ProductService.java
+++ product-ms/src/main/java/br/com/mrocha/productms/service/ProductService.java
@@ -2,9 +2,12 @@ package br.com.mrocha.productms.service;
 
 import br.com.mrocha.productms.dto.ProductDTO;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ProductService {
 
     Optional<ProductDTO> create(ProductDTO request);
+
+    List<ProductDTO> getAll();
 }
