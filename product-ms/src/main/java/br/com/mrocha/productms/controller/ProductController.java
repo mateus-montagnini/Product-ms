6ab8623ab7f74d43cbed2e59bee5f5f3eaed0b25
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-09-09 09:15:36 -0300

    Criando o metodo para a criacao de produtos

---
+++ product-ms/src/main/java/br/com/mrocha/productms/controller/ProductController.java
@@ -0,0 +1,31 @@
+package br.com.mrocha.productms.controller;
+
+import br.com.mrocha.productms.dto.ProductDTO;
+import br.com.mrocha.productms.service.ProductService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService service;
+    @PostMapping
+    public ResponseEntity<ProductDTO> create (@RequestBody @Valid ProductDTO request) {
+        Optional<ProductDTO> response = service.create(request);
+        if (response.isPresent()) {
+            return new ResponseEntity<>(response.get(), HttpStatus.CREATED);
+        }
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+}
