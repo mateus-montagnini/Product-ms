39576453d24b7a4991068b4af47494562d69ff6a
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-09-12 16:01:00 -0300

    Criando o ProductTemplateLoader

---
+++ product-ms/src/test/java/br/com/mrocha/productms/fixture/ProductTemplateLoader.java
@@ -0,0 +1,10 @@
+package br.com.mrocha.productms.fixture;
+
+import br.com.six2six.fixturefactory.loader.TemplateLoader;
+
+public class ProductTemplateLoader implements TemplateLoader {
+    @Override
+    public void load() {
+
+    }
+}
