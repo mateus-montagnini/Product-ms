60b61c7e394cfb37e16d9911a01c891189ea3776
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-09-12 16:07:03 -0300

    Finalizando o ProductTemplateLoader

--- product-ms/src/test/java/br/com/mrocha/productms/fixture/ProductTemplateLoader.java
+++ product-ms/src/test/java/br/com/mrocha/productms/fixture/ProductTemplateLoader.java
@@ -1,10 +1,17 @@
 package br.com.mrocha.productms.fixture;
 
+import br.com.mrocha.productms.dto.ProductDTO;
+import br.com.six2six.fixturefactory.Fixture;
+import br.com.six2six.fixturefactory.Rule;
 import br.com.six2six.fixturefactory.loader.TemplateLoader;
 
 public class ProductTemplateLoader implements TemplateLoader {
     @Override
     public void load() {
-
+        Fixture.of(ProductDTO.class).addTemplate("valid", new Rule(){{
+            add("name", random("Cadeira", "Mesa", "Abajur", "Cama"));
+            add("description", "Descricao do produto com mais de cinquenta caracteres Descricao do produto com mais de cinquenta caracteres Descricao do produto com mais de cinquenta caracteres");
+            add("price", random(Double.class, range(0.01, 9999.99)));
+        }});
     }
 }
