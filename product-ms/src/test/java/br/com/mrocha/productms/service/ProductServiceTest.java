86b26149ea10c72d38637dc83023122f78d44b17
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-09-13 13:57:20 -0300

    Criando teste de buscar todos os produtos

--- product-ms/src/test/java/br/com/mrocha/productms/service/ProductServiceTest.java
+++ product-ms/src/test/java/br/com/mrocha/productms/service/ProductServiceTest.java
@@ -8,10 +8,17 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD;
+
+import java.util.List;
 import java.util.Optional;
 
 @SpringBootTest
+@Sql(executionPhase = BEFORE_TEST_METHOD, scripts = {"classpath:clear-database.sql"})
 public class ProductServiceTest {
 
     @Autowired
@@ -31,5 +38,18 @@ public class ProductServiceTest {
         Assertions.assertEquals(request.getDescription(), response.get().getDescription());
         Assertions.assertEquals(request.getPrice(), response.get().getPrice());
         Assertions.assertTrue(response.get().isAvailable());
+    }
+
+    @Test
+    public void shouldGetAllProducts() {
+        ProductDTO request = Fixture.from(ProductDTO.class).gimme("valid");
+        Optional<ProductDTO> response = service.create(request);
+        List<ProductDTO> responses = service.getAll();
+        System.out.println(">>>>>>>>>> " + responses.size());
+
+        assertNotNull(responses);
+        assertEquals(responses.get(0).getName(), response.get().getName());
+        assertEquals(responses.get(0).getDescription(), response.get().getDescription());
+        Assertions.assertEquals(responses.get(0).getPrice(), response.get().getPrice());
     }
 }
